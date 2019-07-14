85bd9f500756f654f6df54d13dcfe9f57ab4c13d
Aylin DeBruyne <adebruyne88@gmail.com>
2019-07-14 14:18:09 -0400

    adds navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from './components/Home';
+import Navigation from './components/Navigation';
 import './css/App.css';
 function App() {
   return (
     <Router>
+      <Route path="/" component={Navigation} />
       <div className="App">
         <header className="App-header">
           <h1>Recommendation App</h1>
