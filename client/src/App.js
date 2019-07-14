0fc4578df8214163909dbf40e8fc67f9e800f3f0
Aylin DeBruyne <adebruyne88@gmail.com>
2019-07-14 14:01:40 -0400

    adds some mock data

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './css/App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Recommendation App</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Recommendation App</h1>
+        </header>
+      </div>
+
+    </Router>
   );
 }
 
