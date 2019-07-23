85679e2323f72a06dcb5c1389e1be530c29b84c9
Aaron Billings <asbillings2007@gmail.com>
2019-07-23 06:35:39 -0400

    add global error handling

--- server/app.js
+++ server/app.js
@@ -25,7 +25,26 @@ app.get('/', (req, res, next) => {
   });
 });
 // global error handler
-app.use((err, req, res, next) => {});
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  if (err.status >= 100 && err.status < 600) {
+    res.status(err.status).json({ err });
+    console.log(err.status);
+    console.log(err.message);
+    console.log(err.stack);
+  } else {
+    res.status(500).json({ message: 'an internal server error occured' });
+    console.log(500);
+    console.log(err.message);
+    console.log(err.stack);
+  }
+});
 
 // sets port
 const server = process.env.PORT || 5000;
