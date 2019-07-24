9728691f00a155d0a6159eb68daa9dddce5192ea
Aaron Billings <asbillings2007@gmail.com>
2019-07-24 07:12:59 -0400

    create private route

---
+++ client/src/PrivateRoute.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { Consumer } from './Context';
+
+export default ({ component: Component, ...rest }) => {
+  return (
+    <Consumer>
+      {context => (
+        <Route
+          {...rest}
+          render={props =>
+            context.authorizedUser ? (
+              <Component {...props} />
+            ) : (
+              <Redirect
+                to={{ pathname: '/signin', state: { from: props.location } }}
+              />
+            )
+          }
+        />
+      )}
+    </Consumer>
+  );
+};
