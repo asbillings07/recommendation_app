801c60deae5d4e4465ae68f5b0732c251666c124
Aaron Billings <asbillings2007@gmail.com>
2019-07-26 07:18:55 -0400

    add alert form

--- client/src/components/Users/ResetPassword.js
+++ client/src/components/Users/ResetPassword.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Config from '../../Config';
 import Axios from 'axios';
 import UserForm from './UserForm';
-import { Form, Container, Row, Col } from 'react-bootstrap';
+import { Alert, Form, Container, Row, Col, Button } from 'react-bootstrap';
 
 export default class ResetPassword extends Component {
   state = {
@@ -12,7 +12,8 @@ export default class ResetPassword extends Component {
     confirmPassword: '',
     update: false,
     isloading: true,
-    error: '',
+    error: false,
+    errors: '',
   };
 
   async componentDidMount() {
@@ -22,56 +23,81 @@ export default class ResetPassword extends Component {
       params: {
         resetPasswordToken: token,
       },
-    })
-      .then(res => {
-        console.log(res);
-        if (res.data.status === 200) {
-          this.setState({
-            email: res.data.email,
-            update: false,
-            isloading: false,
-          });
-        } else if (res.data.status > 200) {
-          this.setState({
-            update: false,
-            isloading: false,
-            error: 'Invalid Token, please try to reset your password again.',
-          });
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        // this.props.history.push('/error');
-      });
+    }).then(res => {
+      console.log(res);
+      if (res.data.message === 'successful') {
+        this.setState({
+          email: res.data.email,
+          update: false,
+          isloading: false,
+          error: false,
+        });
+      } else {
+        this.setState({
+          update: false,
+          isloading: false,
+          error: true,
+          errors: 'Password Token Expired',
+        });
+      }
+    });
   }
 
   render() {
-    const { password, error, isloading, updated } = this.state;
-    return (
-      <Container>
-        <Row className="justify-content-md-center">
-          <Col xs md lg="auto">
-            <UserForm
-              cancel={this.cancel}
-              errors={error}
-              submit={this.submit}
-              submitButtonText="Update Password"
-              elements={() => (
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Control
-                    type="password"
-                    name="password"
-                    value={password}
-                    placeholder="password"
-                    onChange={this.change}
-                  />
-                </Form.Group>
-              )}
-            />
-          </Col>
-        </Row>
-      </Container>
-    );
+    const { password, error, errors, isloading, updated } = this.state;
+    if (error) {
+      return (
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col xs md lg="auto">
+              <Alert variant="danger">
+                <Alert.Heading>{errors}</Alert.Heading>
+                <p>
+                  Aww shoot! It looks like your reset password token has
+                  expired. This usually happens if you wait too long to follow
+                  the link in your email. No worries, you can still reset your
+                  password with another token. Please press the link below to
+                  get another reset password link. Remember your token does
+                  expire within 24 hours.
+                </p>
+                <hr />
+                <Alert.Link href="/forgotpassword" className="mb-0">
+                  Reset Password
+                </Alert.Link>
+              </Alert>
+            </Col>
+          </Row>
+        </Container>
+      );
+    } else if (isloading) {
+      return null;
+    } else {
+      return (
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col xs md lg="auto">
+              <UserForm
+                cancel={this.cancel}
+                errors={errors}
+                submit={this.submit}
+                submitButtonText="Update Password"
+                elements={() => (
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Control
+                      type="password"
+                      name="password"
+                      value={password}
+                      placeholder="password"
+                      onChange={this.change}
+                    />
+                  </Form.Group>
+                )}
+              />
+            </Col>
+          </Row>
+        </Container>
+      );
+    }
   }
 
   submit = () => {};
