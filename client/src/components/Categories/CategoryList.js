d3dc9be0e45ad8d50205e6cb5cc090cb524fba4d
Aaron Billings <asbillings2007@gmail.com>
2019-07-28 15:49:40 -0400

    add new styles and buttons to category list componet

--- client/src/components/Categories/CategoryList.js
+++ client/src/components/Categories/CategoryList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Axios from 'axios';
 import Config from '../../Config';
 import { Link } from 'react-router-dom';
+import { Container, Button, Row, Col } from 'react-bootstrap';
+import styled from 'styled-components';
 
 class CategoryList extends Component {
   state = {
@@ -25,22 +27,32 @@ class CategoryList extends Component {
 
   showCategories = () => {
     return this.state.categories.map(category => (
-      <React.Fragment key={category.id}>
+      <Col className="mb-2" sm={4} key={category.id}>
         <Link to="#">
-          <h4>{category.title}</h4>
+          <Button variant="primary" size="lg" block>
+            {category.title}
+          </Button>
         </Link>
-      </React.Fragment>
+      </Col>
     ));
   };
 
   render() {
     return (
-      <div>
-        <h5>Browse Categories</h5>
-        {this.showCategories()}
-      </div>
+      <React.Fragment>
+        <H2>Browse Categories</H2>
+        <Container className="mb-7 mt-3">
+          <Row>{this.showCategories()}</Row>
+        </Container>
+      </React.Fragment>
     );
   }
 }
 
 export default CategoryList;
+
+const H2 = styled.h2`
+  display: flex;
+  justify-content: center;
+  font-family: 'Oswald', sans-serif;
+`;
