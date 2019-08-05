3f425c126d6fd050b283e0d428c5cecc2f56fda3
Aaron Billings <asbillings2007@gmail.com>
2019-08-05 08:02:49 -0400

    add user positioning to map

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -9,7 +9,52 @@ class Map extends Component {
       lat: 33.74709,
       lng: -84.35629,
     },
-    zoom: 11,
+    personCoors: {
+      lat: null,
+      lng: null,
+    },
+    zoom: 12,
+  };
+
+  createMapOptions = maps => {
+    // next props are exposed at maps
+    // "Animation", "ControlPosition", "MapTypeControlStyle", "MapTypeId",
+    // "NavigationControlStyle", "ScaleControlStyle", "StrokePosition", "SymbolPath", "ZoomControlStyle",
+    // "DirectionsStatus", "DirectionsTravelMode", "DirectionsUnitSystem", "DistanceMatrixStatus",
+    // "DistanceMatrixElementStatus", "ElevationStatus", "GeocoderLocationType", "GeocoderStatus", "KmlLayerStatus",
+    // "MaxZoomStatus", "StreetViewStatus", "TransitMode", "TransitRoutePreference", "TravelMode", "UnitSystem"
+    return {
+      zoomControlOptions: {
+        position: maps.ControlPosition.RIGHT_CENTER,
+        style: maps.ZoomControlStyle.SMALL,
+      },
+      mapTypeControlOptions: {
+        position: maps.ControlPosition.TOP_RIGHT,
+      },
+      mapTypeControl: true,
+      DirectionsTravelMode: maps.DirectionsTravelMode,
+      DirectionsStatus: maps.DirectionsStatus,
+    };
+  };
+
+  getUserPosition = () => {
+    if ('geolocation' in navigator) {
+      console.log('Geolocation is available');
+      navigator.geolocation.getCurrentPosition(position => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+
+        console.log(lat, lng);
+        this.setState({
+          personCoors: {
+            lat,
+            lng,
+          },
+        });
+      });
+    } else {
+      console.log('geolocation is not avaiable');
+    }
   };
 
   getCoors = address => {
@@ -31,7 +76,7 @@ class Map extends Component {
   };
 
   render() {
-    const { center, zoom } = this.state;
+    const { center, zoom, personCoors } = this.state;
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
@@ -40,9 +85,22 @@ class Map extends Component {
           center={center}
           defaultZoom={zoom}
           onClick={() => this.getCoors(this.props.location)}
+          options={this.createMapOptions}
         >
-          <Marker lat={center.lat} lng={center.lng} text="My Marker" />
+          <Marker
+            lat={center.lat}
+            lng={center.lng}
+            color={'black'}
+            text="My Marker"
+          />
+          <Marker
+            color={'green'}
+            lat={personCoors.lat}
+            lng={personCoors.lng}
+            text="Your location"
+          />
         </GoogleMapReact>
+        {console.log(this.getUserPosition())}
       </div>
     );
   }
