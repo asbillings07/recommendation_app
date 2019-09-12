9948dca07307ab7b46f1fe1b0e1e834c5d1f8095
Aaron Billings <asbillings2007@gmail.com>
2019-09-12 16:05:28 -0400

    fixed data.js issue

--- client/src/Data.js
+++ client/src/Data.js
@@ -43,7 +43,7 @@ export default class Data {
   // create users
   async createUser(user) {
     const response = await this.api('/users', 'POST', user);
-    if (response.status === 201) {
+    if (response.status === 201 || response.status === 200) {
       return [];
     } else if (response.status === 400) {
       return response
