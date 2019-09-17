413c3593d0280e827d13959ed0aa29c6366eb58e
Aaron Billings <asbillings2007@gmail.com>
2019-09-17 15:39:39 -0400

    add hooks to CreateRecommendation component

--- client/src/components/Recommendation/UpdateRecommendation.js
+++ client/src/components/Recommendation/UpdateRecommendation.js
@@ -1,140 +1,124 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import Config from '../../Config';
 import Forms from '../Forms';
 import { Form, Container, Row, Col } from 'react-bootstrap';
 import { notify } from 'react-notify-toast';
 
-export default class UpdateRecommendation extends Component {
-  state = {
-    title: '',
-    description: '',
-    lastVisted: '',
-    location: '',
-    rating: '',
-    userid: '',
-    errors: [],
-    confirmed: true,
-  };
+export default function UpdateRecommendation({ context, match, history }) {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [lastVisted, setLastVisited] = useState('');
+  const [location, setLocation] = useState('');
+  const [rating, setRating] = useState('');
+  const [userid, setUserid] = useState('');
+  const [errors, setErrors] = useState([]);
+  const [confirmed] = useState(true);
 
-  componentDidMount() {
-    this.getData();
-  }
+  useEffect(() => {
+    const getData = async () => {
+      const { id } = match.params;
+      const { authorizedUser } = context;
+      try {
+        const data = await Axios.get(`${Config.apiBaseUrl}/recs/${id}`);
 
-  getData = async () => {
-    const { id } = this.props.match.params;
-    const { authorizedUser } = this.props.context;
-    try {
-      const data = await Axios.get(`${Config.apiBaseUrl}/recs/${id}`);
-
-      if (data) {
-        const rec = data.data;
-        console.log(data.data);
-        this.setState({
-          title: rec.title,
-          description: rec.description,
-          lastVisted: rec.lastvisted,
-          location: rec.location,
-          rating: rec.rating[0],
-          userid: rec.userid,
-        });
-      } else {
-        throw new Error();
+        if (data) {
+          const rec = data.data;
+          setTitle(rec.title);
+          setDescription(rec.description);
+          setLastVisited(rec.lastVisted);
+          setLocation(rec.location);
+          setRating(rec.rating);
+          setUserid(rec.userid);
+        } else {
+          throw new Error('Issue getting data');
+        }
+        console.log(authorizedUser.id);
+        console.log(userid);
+        if (authorizedUser) {
+        } else {
+          history.push('/forbidden');
+        }
+      } catch (err) {
+        console.log(err);
+        history.push('/error');
       }
-      if (authorizedUser.id === this.state.userid) {
-      } else {
-        this.props.history.push('/forbidden');
-      }
-    } catch (err) {
-      console.log(err);
-      this.props.history.push('/error');
-    }
-  };
+    };
 
-  render() {
-    const { title, description, location, errors, confirmed } = this.state;
+    getData();
+  }, [context, history, match.params]);
 
-    return (
-      <Container className="mt-3">
-        <Row className="justify-content-md-center">
-          <Col xs md lg="auto">
-            <>
-              <h1 className="mb-4">Update Your Recommendation</h1>
-              <Forms
-                cancel={this.cancel}
-                errors={errors}
-                submit={this.submit}
-                passwordErrors={confirmed}
-                submitButtonText="Update Recommendation"
-                elements={() => (
-                  <React.Fragment>
-                    <Form.Group>
-                      <Form.Control
-                        type="text"
-                        name="title"
-                        placeholder=""
-                        value={title}
-                        onChange={this.change}
-                      />
-                    </Form.Group>
-                    <Form.Group>
-                      <Form.Control
-                        type="text"
-                        name="description"
-                        placeholder=""
-                        value={description}
-                        onChange={this.change}
-                      />
-                    </Form.Group>
-                    <Form.Group>
-                      <Form.Control
-                        type="text"
-                        name="location"
-                        value={location}
-                        placeholder=""
-                        onChange={this.change}
-                      />
-                    </Form.Group>
-                  </React.Fragment>
-                )}
-              />
-            </>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-
-  change = event => {
-    const name = event.target.name;
-    const value = event.target.value;
-
-    this.setState(() => {
-      return {
-        [name]: value,
-      };
-    });
-  };
-
-  submit = () => {
-    const { id } = this.props.match.params;
-    const { token, data } = this.props.context;
-    const { title, description, location } = this.state;
+  const submit = () => {
+    const { id } = match.params;
+    const { token, data } = context;
     const rec = { title, description, location };
     data
       .updateRecommendation(token, rec, id)
       .then(errors => {
         if (errors.length) {
-          this.setState({ errors: errors.message });
+          setErrors([errors]);
         } else {
           notify.show('Recommendation Updated!', 'success', 10000);
-          this.props.history.push(`/profile`);
+          history.push(`/profile`);
         }
       })
       .catch(error => console.log(error.message));
   };
 
-  cancel = () => {
-    this.props.history.push(`/profile`);
+  const cancel = () => {
+    history.push(`/profile`);
   };
+
+  return (
+    <Container className="mt-3">
+      <Row className="justify-content-md-center">
+        <Col xs md lg="auto">
+          <>
+            <h1 className="mb-4">Update Your Recommendation</h1>
+            <Forms
+              cancel={cancel}
+              errors={errors}
+              submit={submit}
+              passwordErrors={confirmed}
+              submitButtonText="Update Recommendation"
+              elements={() => (
+                <React.Fragment>
+                  <Form.Group>
+                    <Form.Control
+                      aria-label="input the title of your recommendation here"
+                      type="text"
+                      name="title"
+                      placeholder=""
+                      value={title}
+                      onChange={e => setTitle(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Control
+                      aria-label="input the description of your recommendation here"
+                      type="text"
+                      name="description"
+                      placeholder=""
+                      value={description}
+                      onChange={e => setDescription(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Control
+                      aria-label="input the location of your recommendation here"
+                      type="text"
+                      name="location"
+                      value={location}
+                      placeholder=""
+                      onChange={e => setLocation(e.target.value)}
+                    />
+                  </Form.Group>
+                </React.Fragment>
+              )}
+            />
+          </>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
