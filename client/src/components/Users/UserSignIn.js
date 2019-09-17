959c9f416e4c9ade7aa20170837904fea5b9654c
Aaron Billings <asbillings2007@gmail.com>
2019-09-17 09:55:28 -0400

    refactor Usersign with hooks

--- client/src/components/Users/UserSignIn.js
+++ client/src/components/Users/UserSignIn.js
@@ -1,103 +1,79 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Forms from '../Forms';
 import { Form, Container, Row, Col } from 'react-bootstrap';
-//import notify from 'react-notify-toast';
 
-// convert to hooks at somepoint
-export default class UserSignIn extends Component {
-  state = {
-    email: '',
-    password: '',
-    errors: '',
-    confirmed: true,
-  };
+export default function UserSignIn({ context, history, location }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState('');
+  const [confirmed] = useState(true);
 
-  render() {
-    const { email, password, errors, confirmed } = this.state;
+  const submit = () => {
+    const { from } = location.state || { from: { pathname: '/' } };
 
-    return (
-      <Container className="mt-3">
-        <Row className="justify-content-md-center">
-          <Col xs md lg="auto">
-            <h1>Sign In</h1>
-
-            <Forms
-              cancel={this.cancel}
-              errors={errors}
-              submit={this.submit}
-              passwordErrors={confirmed}
-              submitButtonText="Sign In"
-              elements={() => (
-                <React.Fragment>
-                  <Form.Group controlId="formBasicEmail">
-                    <Form.Control
-                      type="email"
-                      name="email"
-                      value={email}
-                      placeholder="name@example.com"
-                      onChange={this.change}
-                    />
-                  </Form.Group>
-
-                  <Form.Group controlId="formBasicPassword">
-                    <Form.Control
-                      type="password"
-                      name="password"
-                      value={password}
-                      placeholder="password"
-                      onChange={this.change}
-                    />
-                  </Form.Group>
-                  <Form.Text className="text-muted mb-2">
-                    <Link to="/forgotpassword">Forgot password?</Link>
-                  </Form.Text>
-                </React.Fragment>
-              )}
-            />
-
-            <p>
-              New to RecommendIt? <Link to="/signup">Sign up</Link>{' '}
-            </p>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-
-  change = e => {
-    const name = e.target.name;
-    const value = e.target.value;
-
-    this.setState(() => {
-      return {
-        [name]: value,
-      };
-    });
-  };
-
-  submit = () => {
-    const { context } = this.props;
-    const { from } = this.props.location.state || { from: { pathname: '/' } };
-    const { email, password } = this.state;
-
-    context.actions
-      .signIn(email, password)
-      .then(user => {
+    try {
+      context.actions.signIn(email, password).then(user => {
         if (user.message === 'ok') {
-          this.props.history.push(from);
+          history.push(from);
         } else {
-          this.setState(() => {
-            return {
-              errors: [user.message],
-            };
-          });
+          setErrors([user.message]);
         }
-      })
-      .catch(err => {});
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  cancel = () => {
-    this.props.history.push('/');
+  const cancel = () => {
+    history.push('/');
   };
+
+  return (
+    <Container className="mt-3">
+      <Row className="justify-content-md-center">
+        <Col xs md lg="auto">
+          <h1>Sign In</h1>
+
+          <Forms
+            cancel={cancel}
+            errors={errors}
+            submit={submit}
+            passwordErrors={confirmed}
+            submitButtonText="Sign In"
+            elements={() => (
+              <React.Fragment>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    value={email}
+                    placeholder="name@example.com"
+                    onChange={e => setEmail(e.target.value)}
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Control
+                    type="password"
+                    name="password"
+                    value={password}
+                    placeholder="password"
+                    onChange={e => setPassword(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Text className="text-muted mb-2">
+                  <Link to="/forgotpassword">Forgot password?</Link>
+                </Form.Text>
+              </React.Fragment>
+            )}
+          />
+
+          <p>
+            New to RecommendIt? <Link to="/signup">Sign up</Link>{' '}
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
