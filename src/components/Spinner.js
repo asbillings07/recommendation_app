b0a0a6b3d00860abf1b4531585802d4b92b99844
asbillings07 <superswiftdeveloper@gmail.com>
2020-08-18 10:46:53 -0400

    update spinner with proptypes

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSync } from '@fortawesome/free-solid-svg-icons';
-import { Container, Row, Col } from 'react-bootstrap';
-import styled from 'styled-components';
+import PropTypes from 'prop-types'
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSync } from '@fortawesome/free-solid-svg-icons'
+import { Container, Row, Col } from 'react-bootstrap'
+import styled from 'styled-components'
 
-export default props => (
-  <Container aria-label="loading page" className="mt-5">
-    <Row className="justify-content-md-center">
-      <Col xs md lg="auto">
-        <FontAwesomeIcon
-          className={`fadeIn ${props.spinning}`}
-          icon={faSync}
-          size={props.size}
-        />
+export const Spinner = ({ size, loadingMsg = 'Loading...' }) => (
+  <Container aria-label='loading page' className='mt-5'>
+    <Row className='justify-content-md-center'>
+      <Col xs md lg='auto'>
+        <FontAwesomeIcon className={`fadeIn spinning`} icon={faSync} size={size} />
         <br />
       </Col>
-      <StyledLoading>{'Loading.....'}</StyledLoading>
+      <StyledLoading>{loadingMsg}</StyledLoading>
     </Row>
   </Container>
-);
+)
+
+Spinner.propTypes = {
+  loadingMsg: PropTypes.string,
+  size: PropTypes.any.isRequired
+}
 
 const StyledLoading = styled.h1`
   margin-top: 3px;
-`;
+`
