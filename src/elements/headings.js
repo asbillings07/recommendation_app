7a4a63cd80b9a211744a4ee553758586374bb53d
asbillings07 <superswiftdeveloper@gmail.com>
2020-11-04 18:57:47 -0500

    updated heading styles

--- src/elements/headings.js
+++ src/elements/headings.js
@@ -1,15 +1,19 @@
 import styled from 'styled-components'
 import { Navbar, Nav } from 'react-bootstrap'
-import { orange, white, black, elevation } from '../utils'
+import { orange, white, black, elevation, fixed } from '../utils'
 
 export const CategoryListHeading = styled.h2`
   display: flex;
   justify-content: center;
   color: ${white};
+  padding: 50px 10px;
+  font-size: 50px;
   font-family: 'Oswald', sans-serif;
 `
 export const NavDiv = styled.div`
   margin-bottom: 0px;
+  ${fixed()}
+  z-index: 100;
 `
 export const NavBar = styled(Navbar)`
   background-color: ${orange} !important;
